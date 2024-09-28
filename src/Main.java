4aed4f3e55f50ca73cd20507bda1ff062c69e838
IanM <ianmarr@Ians-MacBook-Air.local>
2024-09-28 14:49:35 -0400

    InitialCommit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,37 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// First we import the scanner so Java can accept user input.
+// Initialize variables userMonth, which will change to match the user input, and trash, which will interpret incorrect inputs as string variables.
+// Prompt the user to enter a birth year value ranging from 1 to 12.
+// System.out.print("Enter your birth month as a value from 1 to 12.")
+// The program will first run an if statement to check if the user provided an int value. Next, it will check if the int value falls into the range of 1-12.
+// if(in.hasNextInt()) // This checks if the input is a valid integer.
+    // if(userMonth >= 1 & userMonth <= 12) // This checks if the integer, having been deemed valid, falls between 1 and 12.
+
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner in = new Scanner(System.in);
+        int userMonth;
+        String trash;
+            System.out.print("Enter your birth month as a value from 1 to 12: ");
+            if(in.hasNextInt())
+            {
+                userMonth = in.nextInt();
+                if(userMonth >= 1 & userMonth <= 12)
+                {
+                    System.out.print("You were born in month " + userMonth + ".");
+                }
+                else
+                {
+                    System.out.print("The value you provided, " + userMonth + ", is not within the valid month range. Please run the program again and provide a valid month number.");
+                }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+            }
+            else
+            {
+                trash = in.nextLine();
+                System.out.println("The input you provided was " + trash + ", which is not a valid month number. Please run the program again and enter a valid number.");
+            }
+
+
     }
 }
